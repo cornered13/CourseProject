4ae808f8fd56264839daaec38d21983be7ff3943
Osmanov <maccarty845@gmail.com>
2022-04-18 15:12:18 +0300

    Добавьте файлы проекта.

---
+++ CourseProject/VM/ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProject.VM
+{
+    internal class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
